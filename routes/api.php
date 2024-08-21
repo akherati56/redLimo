a38e2c210edefaa987de0ca9b0ce0460ce5a1283
akherati56 <akherati5660@gmail.com>
2024-08-21 10:56:40 +0330

    style(route): cleanup routes and exchange them with resource

--- routes/api.php
+++ routes/api.php
@@ -20,15 +20,9 @@ Route::post('/protect', function (Request $request) {
 })->middleware('auth:api');
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/profile', ProfileController::class);
 });
 
 Route::middleware('auth:api')->group(function () {
-    Route::post('/post/upload', [PostController::class, 'store'])->name('post.upload');
-    Route::get('/post', [PostController::class, 'show'])->name('post.show');
-    Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('/post/{id}', [PostController::class, 'delete'])->name('post.delete');
+    Route::resource('/post', PostController::class);
 });

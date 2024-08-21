6d5eeb419572df37e13d3f19128700ffdefb56d2
akherati56 <akherati5660@gmail.com>
2024-08-21 18:43:31 +0330

    Revert "fix(job): exchange listener with job"
    This reverts commit 0ee31521d934ac7359d46e2a66eadb082c1f6f87.

---
+++ app/Listeners/SendOTP.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserRegistered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Redis;
+
+class SendOTP implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(UserRegistered $event): void
+    {
+        $otp = rand(100000, 999999);
+
+        Redis::setex('otp:' . $event->user->phoneNumber, 600, $otp);
+    }
+}

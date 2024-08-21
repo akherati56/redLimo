8ec6b0b4eca6599a0a4fd47ed2a3fb09f68a500b
akherati56 <akherati5660@gmail.com>
2024-08-21 18:18:42 +0330

    fix(listener): implement queue for sendotp listener

--- app/Listeners/SendOTP.php
+++ app/Listeners/SendOTP.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Redis;
 
-class SendOTP
+class SendOTP implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -22,7 +22,6 @@ class SendOTP
      */
     public function handle(UserRegistered $event): void
     {
-        dd('test');
         $otp = rand(100000, 999999);
 
         Redis::setex('otp:' . $event->user->phoneNumber, 600, $otp);
